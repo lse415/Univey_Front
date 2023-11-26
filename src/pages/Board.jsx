fad95aff2f7c31aa503d28b8cda53e9cd7c9621f
sangyeon <asylee02@gmail.com>
2023-11-27 01:11:18 +0900

    드랍박스 생성

--- src/pages/Board.jsx
+++ src/pages/Board.jsx
@@ -1,12 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import BoardItem from '../components/Board/BoardItem';
 
 export default function Board() {
   const [data,setData]=useState(null);
   const [status,setStatus]=useState(['전체','전체']);
   const [boardData, setboardData] = useState(data);
-  const [complete, setComplete] = useState(null);
-  const [uncomplete, setUnComplete] = useState(null);
   useEffect(()=>{
     dataset()
   },[])
@@ -56,27 +54,34 @@ export default function Board() {
   return (
     <div>
       <nav>
-      <ul className='flex space-x-12 ml-left my-7'>
-        {['전체','IT', '교육', '경제', '사회', '문화'].map((item, index) => (
-          <li
-            key={index}
-            onClick={() => handleItemClick(item)}
-            className={`click_highlight ${status[1]==item ? 'text-highligth' :''}`}
-          >
-            {item}
-          </li>
-        ))}
-      </ul>
-      </nav>
-      <nav>
-      {/* onclick={()=>} */}
-        <ul className='flex space-x-5 ml-left my-7'>
-        <button onClick={()=>handleStatus('전체')} className={`${status[0] === "전체" ? 'select-list-item' : 'common-list-item'}`}>전체</button>
-        <button onClick={()=>handleStatus('uncomplete')}className={`${status[0] === 'uncomplete' ? 'select-list-item' : 'common-list-item'}`}>진행중</button>
-        <button onClick={()=>handleStatus('complete')}className={`${status[0] === "complete" ? 'select-list-item' : 'common-list-item'}`}>완료</button>
+        <div>
+        <ul className='flex space-x-12 ml-left my-7'>
+          {['전체','IT', '교육', '경제', '사회', '문화'].map((item, index) => (
+            <li
+              key={index}
+              onClick={() => handleItemClick(item)}
+              className={`click_highlight ${status[1]==item ? 'text-highligth' :''}`}
+            >
+              {item}
+            </li>
+          ))}
         </ul>
-      </nav>
-
+        </div>
+        <div className='flex justify-between'>
+        {/* onclick={()=>} */}
+          <ul className='flex space-x-5 ml-left my-7'>
+          <button onClick={()=>handleStatus('전체')} className={`${status[0] === "전체" ? 'select-list-item' : 'common-list-item'}`}>전체</button>
+          <button onClick={()=>handleStatus('uncomplete')}className={`${status[0] === 'uncomplete' ? 'select-list-item' : 'common-list-item'}`}>진행중</button>
+          <button onClick={()=>handleStatus('complete')}className={`${status[0] === "complete" ? 'select-list-item' : 'common-list-item'}`}>완료</button>
+          </ul>
+          <select name="" id="" className='h-10 mr-72 mt-5'>
+            <option value="">최신순</option>
+            <option value="">포인트순</option>
+          </select>
+        </div>
+       
+      </nav>    
+     
       <main>
       <div className='w-screen flex flex-col items-center'>
         {(boardData) &&boardData.map((item) => <BoardItem key={item.id} data={item} />)}
