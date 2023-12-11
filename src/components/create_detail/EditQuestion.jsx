6646dfc7c066ca337467dde2214ca8a9a92efbba
lse415 <leesuel0379@gmail.com>
2023-12-12 01:33:02 +0900

    answer  prefill

--- src/components/create_detail/EditQuestion.jsx
+++ src/components/create_detail/EditQuestion.jsx
@@ -7,8 +7,8 @@ import CreateCardTopAsset from '../icons/CreateCardTopAsset';
 
 const EditQuestion = ({ onCancel, onEditQuestion, onRemoveQuestion, initialQuestion, index }) => {
   const [editedQuestion, setEditedQuestion] = useState({ ...initialQuestion });
-  const [answers, setAnswers] = useState(initialQuestion.options || ['']);
-  const [questionType, setQuestionType] = useState(initialQuestion.questionType); 
+  const [answers, setAnswers] = useState(initialQuestion.answers || ['']);
+  const [questionType, setQuestionType] = useState(initialQuestion.question_type);
   const inputRef = useRef(null);
   const [isRequired, setIsRequired] = useState(initialQuestion.isRequired);
 
@@ -16,7 +16,7 @@ const EditQuestion = ({ onCancel, onEditQuestion, onRemoveQuestion, initialQuest
     if (editedQuestion.question.trim() !== '') {
       const updatedQuestion = {
         ...editedQuestion,
-        options: editedQuestion.type === 'multipleChoice' ? answers : null,
+        answers: questionType === 'multipleChoice' ? answers : null,
         isRequired: isRequired,
       };
 
@@ -24,10 +24,7 @@ const EditQuestion = ({ onCancel, onEditQuestion, onRemoveQuestion, initialQuest
     }
   };
 
-    // 삭제 버튼 클릭시 호출
   const handleRemoveQuestion = () => {
-    // onRemoveQuestion 함수를 호출
-    //UserQuestions 컴포넌트로 삭제 요청 전달.
     onRemoveQuestion();
   };
 
@@ -47,7 +44,7 @@ const EditQuestion = ({ onCancel, onEditQuestion, onRemoveQuestion, initialQuest
   const handleClickOutside = (event) => {
     if (inputRef.current) {
       const isInputEmpty = editedQuestion.question.trim() === '';
-  
+
       if (
         !inputRef.current.contains(event.target) &&
         (event.target.className !== 'bg-question_card_bg' ||
@@ -69,71 +66,98 @@ const EditQuestion = ({ onCancel, onEditQuestion, onRemoveQuestion, initialQuest
   return (
     <div className="flex w-full">
       <div className="flex flex-col items-start">
-      <CreateCardTopAsset/>
-      <div ref={inputRef} className="mb-2 w-full rounded p-5 bg-question_card_bg">
-        <div className="flex items-center space-x-4">
-          <input
-            className="w-full p-1 border-none border-question_card_grey bg-transparent text-text_color"
-            placeholder="질문을 작성해주세요"
-            value={editedQuestion.question}
-            onChange={(e) => setEditedQuestion((prev) => ({ ...prev, question: e.target.value }))}
-          />
-          <div className="flex items-center space-x-2">
+        <CreateCardTopAsset />
+        <div ref={inputRef} className="mb-2 w-full rounded p-5 bg-question_card_bg">
+          <div className="flex items-center space-x-4">
             <input
-              id='checked'
-              type="checkbox"
-              value={''}
-              className="mr-1 mt-2 bg-transparent border border-text_color rounded w-4 h-4 "
-              checked={isRequired}
-              onChange={() => {
-                console.log('Checkbox clicked. Current value:', isRequired);
-                setIsRequired(!isRequired);
-              }}
+              className="w-full p-1 border-none border-question_card_grey bg-transparent text-text_color"
+              placeholder="질문을 작성해주세요"
+              value={editedQuestion.question}
+              onChange={(e) => setEditedQuestion((prev) => ({ ...prev, question: e.target.value }))}
             />
-            <label className="mb-0 mt-1">필수</label>
+            <div className="flex items-center space-x-2">
+              <input
+                id="checked"
+                type="checkbox"
+                checked={isRequired}
+                className="mr-1 mt-2 bg-transparent border border-text_color rounded w-4 h-4 "
+                onChange={() => {
+                  console.log('Checkbox clicked. Current value:', !isRequired);
+                  setIsRequired(!isRequired);
+                }}
+              />
+              <label className="mb-0 mt-1">필수</label>
+            </div>
+            <div className="flex items-center mt-2">
+              <select
+                className="p-1 border-b border-question_card_grey bg-transparent text-text_color mr-2"
+                value={questionType}
+                onChange={(e) => setQuestionType(e.target.value)}
+              >
+                <option value="multipleChoice">
+                  <MultipleChoiceIcon className='pr-1' />
+                  객관식
+                </option>
+                <option value="shortAnswer">
+                  <ShortAnswerIcon className='pr-1' />
+                  주관식
+                </option>
+              </select>
+            </div>
           </div>
-          <div className="flex items-center mt-2">
-            <select
-              className="p-1 border-b border-question_card_grey bg-transparent text-text_color mr-2"
-              value={editedQuestion.type}
-              onChange={(e) => setEditedQuestion((prev) => ({ ...prev, type: e.target.value }))}
-            >
-              <option value="multipleChoice">
-                <MultipleChoiceIcon className='pr-1' />객관식
-              </option>
-              <option value="shortAnswer">
-                <ShortAnswerIcon className='pr-1' />주관식
-              </option>
-            </select>
+          {questionType === 'multipleChoice' && (
+            <div className="mt-2">
+              {Array.isArray(initialQuestion.answer) ? (
+                // 배열인 경우
+                initialQuestion.answer.map((answer, index) => (
+                  <div key={index} className="flex items-center mt-2">
+                    <input
+                      className="w-full p-1 border-none border-question_card_grey bg-transparent text-text_color mr-2"
+                      placeholder={'응답 추가'}
+                      value={answer}
+                      onChange={(e) => handleUpdateAnswer(index, e.target.value)}
+                    />
+                  </div>
+                ))
+              ) : (
+                // 객체인 경우
+                Object.values(initialQuestion.answer).map((answer, index) => (
+                  <div key={index} className="flex items-center mt-2">
+                    <input
+                      className="w-full p-1 border-none border-question_card_grey bg-transparent text-text_color mr-2"
+                      placeholder={'응답 추가'}
+                      value={answer}
+                      onChange={(e) => handleUpdateAnswer(index, e.target.value)}
+                    />
+                  </div>
+                ))
+              )}
+            </div>
+          )}
+          {questionType === 'shortAnswer' && (
+            <div className="mt-4">
+              <input
+                type="text"
+                className="w-full p-1 border rounded border-question_card_grey bg-white text-text_color mr-2"
+                placeholder={'주관식 서술 문항입니다. 자유롭게 작성해주세요.'}
+                value={answers[0]}
+                disabled
+              />
+            </div>
+          )}
+          <hr className='my-4 py-2 border-question_card_grey'></hr>
+          <div className='flex justify-end mt-2'>
+            <button className='px-2'>
+              <CopyButtonIcon />
+            </button>
+            <button className='pl-2' onClick={handleRemoveQuestion}>
+              <DeleteButtonIcon />
+            </button>
           </div>
         </div>
-        {questionType === 'multipleChoice' && (
-          <div className="mt-2">
-            {answers.map((answer, index) => (
-              <div key={index} className="flex items-center mt-2">
-                <input
-                  className="p-1 border-none border-question_card_grey bg-transparent text-text_color mr-2"
-                  placeholder={'응답 추가'} 
-                  value={answer}
-                  onChange={(e) => handleUpdateAnswer(index, e.target.value)}
-                />
-              </div>
-            ))}
-          </div>
-        )}
-        <hr className='my-4 py-2 border-question_card_grey'></hr>
-        <div className='flex justify-end mt-2'>
-          <button className='px-2'>
-            <CopyButtonIcon />
-          </button>
-          <button className='pl-2' onClick={handleRemoveQuestion}>
-            <DeleteButtonIcon />
-          </button>
-        </div>
-      </div>
       </div>
     </div>
-  )
+  );
 };
 
 export default EditQuestion;
