0a935439cb95f6c3451b4bb329cc3cbb10b07470
sangyeon <asylee02@gmail.com>
2024-01-04 17:18:26 +0900

    머지 합치기

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import CreateSurvey from "../components/create/CreateSurvey"
+import CreateSurvey from "../components/Create/CreateSurvey"
 import { GoPencil } from "react-icons/go";
 
 const Create = () => {
