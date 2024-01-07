6ea6d660de9675e5006e3bdcd44a4f4ed0ee2ddf
lse415 <leesuel0379@gmail.com>
2024-01-07 23:16:03 +0900

    질문생성 answer 유효성 검사

--- src/components/create_detail/UserQuestions.jsx
+++ src/components/create_detail/UserQuestions.jsx
@@ -1,15 +1,14 @@
-import React, { useState, useRef, useEffect } from 'react';
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
-import QuestionCard from './QuestionCard';
-import CreateQuestion from './CreateQuestion';
-import EditQuestion from './EditQuestion';
-import { BiSolidQuoteAltLeft } from 'react-icons/bi';
-import { BiSolidQuoteAltRight } from 'react-icons/bi';
-import AddButtonIcon from '../icons/AddButtonIcon';
+import React, { useState, useRef, useEffect } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import QuestionCard from "./QuestionCard";
+import CreateQuestion from "./CreateQuestion";
+import EditQuestion from "./EditQuestion";
+import { BiSolidQuoteAltLeft } from "react-icons/bi";
+import { BiSolidQuoteAltRight } from "react-icons/bi";
+import AddButtonIcon from "../icons/AddButtonIcon";
 
 const UserQuestions = ({
-  surveyId,
   userQuestions,
   setUserQuestions,
   onAddQuestion,
@@ -20,33 +19,22 @@ const UserQuestions = ({
   const [creatingQuestion, setCreatingQuestion] = useState(false);
   const [editingIndex, setEditingIndex] = useState(null);
   const inputRef = useRef(null);
-  const [question, setQuestion] = useState('');
-  const [answer, setAnswer] = useState(['']); 
+  const [question, setQuestion] = useState("");
+  const [answer, setAnswer] = useState([""]);
   const [accessToken, setAccessToken] = useState("");
-
-  const mock ={    
-    "userQuestions": [        
-    {            
-        "question_num": 1,                      
-        "question": " 인공지능 사용 빈도가 어떻게 되나요?",                       "question_type":"multipleChoice",              
-        "isRequired": true,               "answer": ["매우 자주", "자주", "가끔", "거의 사용하지 않음"
-    ]        
-    },
-    {           
-        "question_num": 2,             
-        "question": " 어떤 종류의 인공지능 기술을 사용하나요?",            
-       "question_type": "shortAnswer",             
-       "isRequired": false,            "answer": []
-    }    
-    ]
-    }
+  const [isValid, setIsValid] = useState(
+    Array(userQuestions.length).fill(false)
+  );
 
   const navigate = useNavigate();
 
   const handleAddQuestion = (question) => {
     if (editingIndex !== null) {
       const updatedQuestions = [...userQuestions];
-      updatedQuestions[editingIndex] = { ...question, question_num: editingIndex + 1 };
+      updatedQuestions[editingIndex] = {
+        ...question,
+        question_num: editingIndex + 1,
+      };
       onEditQuestion(updatedQuestions);
     } else {
       const newQuestion = {
@@ -57,11 +45,11 @@ const UserQuestions = ({
         answer: question.answer || {},
       };
 
-      console.log('New Question:', newQuestion);
-      onAddQuestion(newQuestion);  // 직접 newQuestion 전달
+      console.log("New Question:", newQuestion);
+      onAddQuestion(newQuestion); // 직접 newQuestion 전달
 
-      setQuestion('');
-      setAnswer(['']);
+      setQuestion("");
+      setAnswer([""]);
     }
 
     setCreatingQuestion(false);
@@ -71,7 +59,10 @@ const UserQuestions = ({
   const handleCopyCreateQuestion = (question) => {
     if (editingIndex !== null) {
       const updatedQuestions = [...userQuestions];
-      updatedQuestions[editingIndex] = { ...question, question_num: editingIndex + 1 };
+      updatedQuestions[editingIndex] = {
+        ...question,
+        question_num: editingIndex + 1,
+      };
       onEditQuestion(updatedQuestions);
     } else {
       const newQuestion = {
@@ -81,14 +72,13 @@ const UserQuestions = ({
         isRequired: question.isRequired,
         answer: question.answer || {},
       };
-      onAddQuestion(newQuestion);  // 직접 newQuestion 전달
+      onAddQuestion(newQuestion); // 직접 newQuestion 전달
     }
 
     setCreatingQuestion(true);
   };
 
-  const handleCopyEditQuestion = () => {
-  };
+  const handleCopyEditQuestion = () => {};
 
   const handleUpdateQuestion = (updatedQuestion, index) => {
     const updatedQuestions = [...userQuestions];
@@ -96,7 +86,7 @@ const UserQuestions = ({
 
     setUserQuestions(updatedQuestions);
 
-    if (typeof onEditQuestion === 'function') {
+    if (typeof onEditQuestion === "function") {
       onEditQuestion(updatedQuestions);
     }
 
@@ -106,6 +96,11 @@ const UserQuestions = ({
   const handleEditQuestion = (index) => {
     setCreatingQuestion(false);
     setEditingIndex(index === editingIndex ? null : index);
+    setIsValid((prevIsValid) => {
+      const newIsValid = [...prevIsValid];
+      newIsValid[index] = true;
+      return newIsValid;
+    });
   };
 
   // 선택한 질문 삭제
@@ -118,7 +113,7 @@ const UserQuestions = ({
       ...question,
       question_num: i + 1,
     }));
-    
+
     // setUserQuestions 함수를 사용하여 상위 컴포넌트의 userQuestions 상태를 업데이트
     setUserQuestions(updatedQuestionsWithNum);
     // 현재 편집 중인 질문이 있을 경우 편집 상태 종료
@@ -131,11 +126,11 @@ const UserQuestions = ({
   };
 
   const handleClickOutside = (event) => {
-    const inputElement = inputRef.current?.querySelector('input');
+    const inputElement = inputRef.current?.querySelector("input");
 
     if (
       inputElement &&
-      inputElement.value.trim() === '' &&
+      inputElement.value.trim() === "" &&
       !inputRef.current.contains(event.target) &&
       (creatingQuestion || editingIndex !== null)
     ) {
@@ -145,29 +140,42 @@ const UserQuestions = ({
   };
 
   const handleSubmit = () => {
+    const isValidArray = userQuestions.map((userQuestion) => {
+      return userQuestion.question_type === "multipleChoice"
+        ? userQuestion.answer.length > 0
+        : true;
+    });
 
-    console.log(userQuestions)
-    localStorage.setItem('userQuestions', JSON.stringify({userQuestions}));
-    console.log()
-    navigate(`/main/create/qr/${surveyId}`);
+    setIsValid(isValidArray);
 
+    // 유효성이 모두 true일 경우에만 submit 처리
+    if (isValidArray.every((isValid) => isValid)) {
+      localStorage.setItem("userQuestions", JSON.stringify({ userQuestions }));
+      navigate("main/create/qr");
+    }
   };
 
   useEffect(() => {
-    document.addEventListener('mousedown', handleClickOutside);
+    // 초기에는 모든 질문이 유효하다고 가정...
+    const initialIsValidArray = Array(userQuestions.length).fill(true);
+    setIsValid(initialIsValidArray);
+
+    document.addEventListener("mousedown", handleClickOutside);
 
     return () => {
-      document.removeEventListener('mousedown', handleClickOutside);
+      document.removeEventListener("mousedown", handleClickOutside);
     };
-  }, [creatingQuestion, editingIndex]);
+  }, [creatingQuestion, editingIndex, userQuestions]);
 
-  console.log('userQuestions:', userQuestions); 
+  console.log("userQuestions:", userQuestions);
 
   return (
     <div className="flex-1 p-4 mt-20">
       <div className="flex justify-center">
-      <BiSolidQuoteAltLeft className="text-main_color" />
-        <h2 className="text-2xl font-semibold text-main_color mb-4 px-2">{topic}</h2>
+        <BiSolidQuoteAltLeft className="text-main_color" />
+        <h2 className="text-2xl font-semibold text-main_color mb-4 px-2">
+          {topic}
+        </h2>
         <BiSolidQuoteAltRight className="text-main_color" />
       </div>
       <p className="text-center text-sm mb-7">{description}</p>
@@ -176,16 +184,18 @@ const UserQuestions = ({
         <div key={index}>
           {editingIndex === index ? (
             <EditQuestion
-            onCancel={handleClick}
-            onEditQuestion={(updatedQuestion) => handleUpdateQuestion(updatedQuestion, index)}
-            onCopyEditQuestion={(copiedQuestion) => 
-              handleCopyEditQuestion(copiedQuestion, index)
-            }       
-            onRemoveQuestion={() => handleRemoveQuestion(index)}     
-            initialQuestion={userQuestion}
-            userQuestions={userQuestions}
-            setUserQuestions={setUserQuestions}
-          />
+              onCancel={handleClick}
+              onEditQuestion={(updatedQuestion) =>
+                handleUpdateQuestion(updatedQuestion, index)
+              }
+              onCopyEditQuestion={(copiedQuestion) =>
+                handleCopyEditQuestion(copiedQuestion, index)
+              }
+              onRemoveQuestion={() => handleRemoveQuestion(index)}
+              initialQuestion={userQuestion}
+              userQuestions={userQuestions}
+              setUserQuestions={setUserQuestions}
+            />
           ) : (
             <QuestionCard
               question={userQuestion.question}
@@ -194,38 +204,40 @@ const UserQuestions = ({
               answer={userQuestion.answer}
               onClick={() => handleEditQuestion(index)}
               isEditing={editingIndex !== null}
+              isValid={isValid[index]}
             />
           )}
         </div>
       ))}
       <div className="mt-4 flex" ref={inputRef}>
         {creatingQuestion ? (
-          <CreateQuestion 
-            onCancel={handleClick} 
-            onAddQuestion={handleAddQuestion} 
-            onCopyCreateQuestion={handleCopyCreateQuestion}/>
+          <CreateQuestion
+            onCancel={handleClick}
+            onAddQuestion={handleAddQuestion}
+            onCopyCreateQuestion={handleCopyCreateQuestion}
+          />
         ) : (
           <div className="flex items-center w-full">
             <button className="text-main_color pr-2 font-bold p-2 mb-4">
-                <AddButtonIcon />
+              <AddButtonIcon />
             </button>
             <div
-                onClick={handleClick}
-                className="flex-grow p-2 mb-4 bg-question_card_bg rounded text-sub_text_color cursor-pointer"
+              onClick={handleClick}
+              className="flex-grow p-2 mb-4 bg-question_card_bg rounded text-sub_text_color cursor-pointer"
             >
-                문항을 선택해 주세요.
+              문항을 선택해 주세요.
             </div>
-        </div>
+          </div>
         )}
       </div>
-      <div className='text-right mt-10'>
-        <button 
-          className='px-9 py-1 rounded-xl border border-sub_text_color_4 text-sub_text_color_4'
+      <div className="text-right mt-10">
+        <button
+          className="px-9 py-1 rounded-xl border border-sub_text_color_4 text-sub_text_color_4"
           onClick={handleSubmit}
-          >
+        >
           다음
         </button>
-    </div>
+      </div>
     </div>
   );
 };
